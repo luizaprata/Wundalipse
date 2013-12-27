6cd5f0ae9d2fe1d86abc6e8e03c6238a068802fe
Luiza Prata Soldi Passos <luiza.psp@gmail.com>
2013-12-27 01:47:31 -0200

    inserindo char, movimento teclado

--- games/wireless_fiction/src/scripts/WirelessFiction.js
+++ games/wireless_fiction/src/scripts/WirelessFiction.js
@@ -4,21 +4,22 @@
     function WirelessFiction() {}
 
 	var s = WirelessFiction;
-	s.PADDLE_SPEED = 25;
+	s.CHAR_SPEED = 25;
 	s.GAME_TIME = 30 * 1000;
-    
-    
 
     WirelessFiction.prototype = {
         
-       // Check out the documentation for details on the game format.
-
 	    // Passed in via initialize
 	    stage: null,
 	    assets: null,
 	    gameInfo: null,
+        
         floors: null,
         floor: null,
+        floor_h: 200,
+        floor_w: 800,
+        gameContainer: null,
+        character: null,
 
 	    // On-screen objects
 
@@ -34,9 +35,20 @@
 		    this.gameInfo = gameInfo;
             this.floors = assets.data_floors.floors;
             
-
+            
+            this.gameContainer = new createjs.Container();
+            //this.gameContainer.vy = .01;
+		    this.stage.addChild(this.gameContainer);
+        
+            
 			var bg = GameLibs.GameUI.changeBackground(assets.background, gameInfo.width, gameInfo.height, "stretch");
-		    stage.addChild(bg);
+		    this.gameContainer.addChild(bg);
+            
+            this.character = GameLibs.GameUI.changeBackground(assets.char, 100, 116, "stretch");
+            this.character.x = 100;
+            this.character.y = 100;
+            this.gameContainer.addChild(this.character);
+
             
 		    // Add a HUD background
 		    //var hud = new createjs.Shape(new createjs.Graphics().beginFill("rgba(255,0,0,0.4)").drawRect(0,0,this.gameInfo.width,55));
@@ -44,15 +56,19 @@
 	    },
         
         buildFloor: function() {
+            
+            
             for (var n in this.floors) {
                 
                 this.floor = new createjs.Shape(
-                    new createjs.Graphics().f("rgba(255,0,0,0.4)").dr((this.gameInfo.width-800)/2,0,800,100)
+                    new createjs.Graphics().f("rgba(0,0,0,0.4)").dr(0,0,this.floor_w,this.floor_h)
                 );
-                this.floor.y = this.gameInfo.height - ((parseInt(n)+1)*100);
-                console.info(this.floor.y);
-                this.stage.addChild(this.floor);
+                this.floor.x = (this.gameInfo.width-this.floor_w)/2;
+                this.floor.y = this.gameInfo.height - (parseInt(n)+1)*this.floor_h;
+                this.gameContainer.addChild(this.floor);
             }
+            
+            //this.gameContainer.y = 00;
         },
 
 	    startGame: function () {
@@ -68,6 +84,20 @@
         //MUST HAVE//"tick", "pause", "getScore", "continueGame", "restart", "destroy"
         
         tick: function (tickFactor) {
+            
+            this.playerMove();
+        },
+        
+        playerMove: function () {
+            var pad = GameLibs.GamePad;
+		    var player = pad.player;
+            
+            // keyboard event
+            if (player.isButtonDown(pad.LEFT)) {
+			    this.character.x -= s.CHAR_SPEED;
+		    } else if (player.isButtonDown(pad.RIGHT)) {
+			    this.character.x += s.CHAR_SPEED;
+		    }
         },
         pause: function (paused) {
 
