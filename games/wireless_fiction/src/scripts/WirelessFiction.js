caa048b0da53445fb6fffbf6a6b89722081a8e31
Luiza Prata Soldi Passos <luiza.psp@gmail.com>
2013-12-26 23:37:58 -0200

    lendo json contruindo cenario

--- games/wireless_fiction/src/scripts/WirelessFiction.js
+++ games/wireless_fiction/src/scripts/WirelessFiction.js
@@ -1,5 +1,5 @@
 
-(function(scope) {
+(function (scope) {
 
     function WirelessFiction() {}
 
@@ -17,55 +17,71 @@
 	    stage: null,
 	    assets: null,
 	    gameInfo: null,
+        floors: null,
+        floor: null,
 
 	    // On-screen objects
 
-	    timeRemaining:s.GAME_TIME, // Remaining game time in milliseconds
+	    timeRemaining: s.GAME_TIME, // Remaining game time in milliseconds
 	    startTime: 0,           // Time that the game started
 	    hits: 0,                // Number of hits so far
 
 	    scoreManager: null,     // Tracks the score
 
-	    initialize: function(assets, stage, gameInfo) {
+	    initialize: function (assets, stage, gameInfo) {
 		    this.assets = assets;
 		    this.stage = stage;
 		    this.gameInfo = gameInfo;
+            this.floors = assets.data_floors.floors;
+            
 
 			var bg = GameLibs.GameUI.changeBackground(assets.background, gameInfo.width, gameInfo.height, "stretch");
 		    stage.addChild(bg);
             
 		    // Add a HUD background
-		    var hud = new createjs.Shape(new createjs.Graphics().beginFill("rgba(255,0,0,0.4)").drawRect(0,0,this.gameInfo.width,55));
-		    this.stage.addChild(hud);
-	    },
-
-	    startGame: function() {
-
-		    
+		    //var hud = new createjs.Shape(new createjs.Graphics().beginFill("rgba(255,0,0,0.4)").drawRect(0,0,this.gameInfo.width,55));
+		    //this.stage.addChild(hud);
 	    },
         
-	    gameOver: function() {
+        buildFloor: function() {
+            for (var n in this.floors) {
+                
+                this.floor = new createjs.Shape(
+                    new createjs.Graphics().f("rgba(255,0,0,0.4)").dr((this.gameInfo.width-800)/2,0,800,100)
+                );
+                this.floor.y = this.gameInfo.height - ((parseInt(n)+1)*100);
+                console.info(this.floor.y);
+                this.stage.addChild(this.floor);
+            }
+        },
+
+	    startGame: function () {
+            
+		    this.buildFloor();
+	    },
+        
+	    gameOver: function () {
 		    // We could do something fancy here...
 			this.onGameOver();
 	    },
 
         //MUST HAVE//"tick", "pause", "getScore", "continueGame", "restart", "destroy"
         
-        tick: function(tickFactor) {
+        tick: function (tickFactor) {
         },
-        pause: function(paused) {
+        pause: function (paused) {
 
 	    },
-       getScore: function() {
+       getScore: function () {
 		    //return new GameLibs.GameDetails(this.scoreManager.score);
 	    },
-        continueGame: function(keepPoints) {
+        continueGame: function (keepPoints) {
 		  //  if (!this.keepPoints) {
 			 //   this.scoreManager.setScore(0);
 		    //}
 			//this.startLevel();
 	    },
-        restart: function() {
+        restart: function () {
 		    //this.scoreManager.setScore(0);
 		    //this.startLevel();
 	    }
